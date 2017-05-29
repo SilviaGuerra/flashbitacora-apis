f0befd9674ce4d4f1909bdc78aca201b7e0c9da5
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-05-29 00:00:24 -0500

    imprime titulo y mensajes nuevos

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,16 @@ publicarMensaje.addEventListener("click", mensajeNuevo);
 
 function mensajeNuevo(){
   var contenedor = document.getElementById("publicaciones");
-
   var mensajeTitulo = document.getElementById("titulo").value;
   var nuevoTitulo = document.createElement("h4");
-  // nuevoTitulo.innerHTML = mensajeTitulo;
-  contenedor.nuevoTitulo.appendChild(document.createTextNode(mensajeTitulo));
+  nuevoTitulo.innerText = mensajeTitulo;
+  contenedor.appendChild(nuevoTitulo);
+  document.getElementsByClassName("input").value = "";
 
-  // var mensajeTexto = document.getElementById("mensaje").value;
-  //
-  // contenedor = document.createElement("div");
-  // var nuevoMensaje = document.createTextNode("p");
-  // mensajeNuevo.innerHTML = mensajeTitulo;
-}
+  var mensajeTexto = document.getElementById("mensaje").value;
+  var nuevoTexto = document.createElement("p");
+  nuevoTexto.innerText = mensajeTexto;
+  contenedor.appendChild(nuevoTexto);
+};
+
+mensajeNuevo();
