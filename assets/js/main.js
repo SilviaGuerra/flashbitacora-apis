e33f5890ede5698c2cd4967c7e571f69df668457
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-05-28 21:32:37 -0500

    aparece modal sin publicarse

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+// var mensajeTitulo = document.getElementById("titulo");
+// var mensajeTexto = document.getElementById("mensaje");
+// var botonPublicar = document.getElementById("publicar");
+// var publicaciones = document.getElementById("publicaciones");
+//
+// botonPublicar.addEventListerner("click", publicar);
+//
+// function publicar(){
+//   publicaciones = document.createElement("div");
+//   var titulo = document.createElement("h4");
+//   var tituloIngresar = document.createTextNode(mensajeTitulo.value);
+//   var mensaje = document.createElement("p");
+//   var mensajeIngresar = document.createTextNode(mensajeTexto.value);
+//
+//   titulo.appendChild(tituloIngresar);
+//   contenedor.appendChild(titulo);
+//   titulo.style.color="pink";
+//
+//   mensaje.appendChild(mensajeIngresar);
+//   mensajesPublicados.appendChild(mensaje);
+//   mensaje.style.color="grey";
+// }
+
+function agregarComentario(){}
